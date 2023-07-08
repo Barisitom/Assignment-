dc28fcde399ef07dc24356ef1abad24398c1394e
Barisitom <akpeebarisitom@gmail.com>
2023-07-08 15:22:37 -0700

    add light.py

---
+++ light.py
@@ -0,0 +1,17 @@
+print('\nPlease enter the fellowing :\n')
+
+adjective = input('adjective: ')
+animal = input('animal: ')
+verb = input('verb: ')
+exclamation = input('exclamation: ')
+verb_b = input('verb: ')
+verb_c = input('verb: ')
+
+
+print( '\nYour story is:\n\n'
+
+#I formated the string using different form i leant
+
+    f'The other day, I was really in trouble. It all started when I saw a very {adjective} {animal} {verb} down the hallway.\n'
+    +'"'+ exclamation.capitalize() +'!'+'" '+ 'I yelled. But all I could think to do was to'+' '+ verb_b+' '+'over and over. ' 'Miraculously, that caused it to stop,\n but not before it tried to {} right in front of my family.\n'.format(verb_c)  
+    )
